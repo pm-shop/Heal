8c4a775f6674239756a09fa5a1df56cf204a69bf
Pralexkid <40715162+Pralexkid@users.noreply.github.com>
2021-03-20 21:05:38 +0100

    Update Main.php

--- src/heal/Main.php
+++ src/heal/Main.php
@@ -13,11 +13,13 @@ class main extends PluginBase
 {
     public function onEnable()
     {
+        $this->getServer()->getLogger()->info("For PM Shop - By Pralexkid");
         @mkdir($this->getDataFolder());
         $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
             "healMessageCible" => "§4[Heal] §aVous avez ete heal par _op_ !",
             "healMessageCibleConsole" => "§4[Heal] §aVous avez ete heal par la console !",
-            "healMessageOp" => "§4[Heal] §aVous avez heal _cible_ !"
+            "healMessageOp" => "§4[Heal] §aVous avez heal _cible_ !",
+            "healSelfMessage" => "§4[Heal] §aVous venez de vous heal !"
         ));
         $this->saveResource("config.yml");
     }
@@ -36,14 +38,17 @@ class main extends PluginBase
                                 $player->setHealth(20);
                                 $player->sendPopup($this->getConfig()->get("healMessageCible"));
                                 $sender->sendPopup($this->getConfig()->get("healMessageOp"));
-                            } else {
-                                $sender->sendPopup("§4[Heal] §cVous ne pouvez pas vous heal !");
                             }
                         } else {
                             $sender->sendMessage("§4[Heal] §cVeuillez indiquer un nom de joueur valide !");
                         }
                     }  else {
-                        $sender->sendMessage("§4[Heal] §cVeuillez indiquer un nom de joueur valide !");
+                        if ($sender->hasPermission("heal.player")) {
+                            $sender->setHealth(20);
+                            $sender->sendPopup($this->getConfig()->get("healSelfMessage"));
+                        } else {
+                            $sender->sendMessage("§cVous n'avez pas la permission !");
+                        }
                     }
                         } elseif ($sender instanceof ConsoleCommandSender) {
                     if (isset($args[0])) {
@@ -60,6 +65,8 @@ class main extends PluginBase
                         $this->getServer()->getLogger()->info("Veuillez indiquer un nom de joueur valide !");
                     }
                 }
+            } else {
+                $sender->sendMessage("§cVous n'avez pas la permission !");
             }
         } return true;
     }
